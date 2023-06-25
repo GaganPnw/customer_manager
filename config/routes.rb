b9a90b8cf8c500815ec084bcb84b8108b3136da5
gagandeep <gagan.panwar2004@gmail.com>
2023-06-25 11:05:18 -0500

    Updated partial path and added styles for customer views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'customers#index'
+  get '/customers/missing_email', to: 'customers#missing_email'
+  get '/customers/alphabetized', to: 'customers#alphabetized'
 end
